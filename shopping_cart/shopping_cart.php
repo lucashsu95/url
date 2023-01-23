5f7e51d5ef7e25902e27029625333d6edb0f2b81
lucashsu95 <lucashsu95@gmail.com>
2023-01-23 18:48:32 +0800

    2023/1/23

--- shopping_cart/shopping_cart.php
+++ shopping_cart/shopping_cart.php
@@ -1,5 +1,4 @@
 <?php
-// mysql
 $db = new PDO('mysql:host=localhost;dbname=test','admin','1234');
 $sql = $db->prepare('insert into uorder(product1,product2,product3,product4,product5,product6,SumMoney) values(:pd1,:pd2,:pd3,:pd4,:pd5,:pd6,:sum)');
 $i = 0;
