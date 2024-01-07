b5b1bd859e15943617bc3d0c740b4bf02391c62f
lucashsu95 <96901075+lucashsu95@users.noreply.github.com>
2024-01-07 04:26:08 +0000

    Update file

--- js/index.js
+++ js/index.js
@@ -42,14 +42,14 @@ const app = Vue.createApp({
                 {
                     title: '<img src="./images/icon.png" class="icon" />減碳大作戰',
                     description: '遊戲內容主要是以玩家操控角色躲避 高碳食物吃到低碳食物，介由玩遊戲的方式讓玩家了解到什麼是低碳食物什麼是高碳食物。',
-                    href: './減碳大作戰1/減碳大作戰.html',
+                    href: './減碳大作戰/減碳大作戰.html',
                     download: '',
                     copy: '',
                 },
                 {
                     title: '<img src="./images/icon.png" class="icon" />AI減碳相機',
                     description: '可以使用網站的攝像頭 來去辨識出是什麼食物，並且呈現出一公克排出多少的碳。',
-                    href: './減碳相機1/減碳相機.html',
+                    href: './減碳相機/減碳相機.html',
                     download: '',
                     copy: '',
                 },
@@ -65,20 +65,6 @@ const app = Vue.createApp({
                     description: '如果資料夾或是桌面太過雜亂...沒關係! 只要使用檔案分頪器 只要丟到想要分類的資料夾在按下bat檔即可開始',
                     href: '',
                     download: './file/檔案分類器.zip',
-                    copy: '',
-                },
-                {
-                    title: '<img src="./images/text.png" class="icon" />vscode字體樣式',
-                    description: '好看護眼的IntelOne Mono字體',
-                    href: '',
-                    download: './file/style.zip',
-                    copy: '',
-                },
-                {
-                    title: '<img src="./images/python.png" class="icon" />距離目標日期還有多久.py',
-                    description: '執行bat檔後，會出一現一個彈跳視窗，要改目標日期要到程式碼裡改喔!',
-                    href: '',
-                    download: './file/date_distance.zip',
                     copy: '',
                 },
             ]
