8ed36a47f0d3d662403c71f06f4b18e0a1556a48
lucashsu95 <lucashsu95@gmail.com>
2024-01-23 19:20:30 +0800

    Update product class function

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ const app = Vue.createApp({
             products: [
                 {
                     title: '<img src="./images/html.png" class="icon" />TodoList',
+                    class: ['web'],
                     description: '一個可支援txt檔的TodoList,使用了LocalStorage,所以沒辦法存留太久 :D',
                     href: './todoList',
                     download: '',
@@ -34,6 +35,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '<img src="./images/icon.png" class="icon" />碳計算App',
+                    class: ['tool', 'appinventor'],
                     description: '為了讓我們可以簡單得知我們一餐含多少碳足跡，於是我們製作了碳計算App可以輸入公克數目的方式來獲得碳足跡',
                     href: '',
                     download: './file/排碳計算機App-en/carbon.apk',
@@ -41,6 +43,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '<img src="./images/icon.png" class="icon" />減碳大作戰',
+                    class: ['game', 'Construct2'],
                     description: '遊戲內容主要是以玩家操控角色躲避 高碳食物吃到低碳食物，介由玩遊戲的方式讓玩家了解到什麼是低碳食物什麼是高碳食物。',
                     href: './減碳大作戰/減碳大作戰.html',
                     download: '',
@@ -48,6 +51,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '<img src="./images/icon.png" class="icon" />AI減碳相機',
+                    class: ['tool', 'teachable'],
                     description: '可以使用網站的攝像頭 來去辨識出是什麼食物，並且呈現出一公克排出多少的碳。',
                     href: './減碳相機/減碳相機.html',
                     download: '',
@@ -55,6 +59,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '<img src="./images/js.png" class="icon" />n.js',
+                    class: ['js'],
                     description: '這是為了簡單重複html元素的一個小功具，只要在html元素上打上n-for="3"裡面的內容就會被斷重複的產生3次',
                     href: '',
                     download: './file/n.js',
@@ -62,6 +67,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '<img src="./images/python.png" class="icon" />檔案分類器.py',
+                    class: ['python'],
                     description: '如果資料夾或是桌面太過雜亂...沒關係! 只要使用檔案分頪器 只要丟到想要分類的資料夾在按下bat檔即可開始',
                     href: '',
                     download: './file/檔案分類器.zip',
@@ -69,6 +75,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '筆劃練習.exe',
+                    class: ['smartApp'],
                     description: '這是一個用smart app 軟體製做的，教你寫我的名字的軟體，這樣因該就不會有人寫錯字了吧?是「綸」啦!這是電腦可以用的.exe檔',
                     href: '',
                     download: 'file/筆劃練習/筆劃練習.exe',
@@ -76,6 +83,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '筆劃練習.apk',
+                    class: ['smartApp'],
                     description: '這是一個用smart app 軟體製做的，教你寫我的名字的軟體，這樣因該就不會有人寫錯字了吧?是「綸」啦!這是android手機可以用的.apk檔，iphone的話不太行喔',
                     href: '',
                     download: 'file/筆劃練習/筆劃練習.apk',
@@ -83,6 +91,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '貓貓拼圖.apk',
+                    class: ['smartApp'],
                     description: '這是一個用smart app 軟體製做的，簡單的拼圖遊戲，不用管遊戲結束的那一張圖，這是電腦可以用的.exe檔',
                     href: '',
                     download: 'file/貓貓拼圖/貓貓拼圖.apk',
@@ -90,6 +99,7 @@ const app = Vue.createApp({
                 },
                 {
                     title: '貓貓拼圖.apk',
+                    class: ['smartApp'],
                     description: '這是一個用smart app 軟體製做的，簡單的拼圖遊戲，不用管遊戲結束的那一張圖，這是android手機可以用的.apk檔，iphone的話不太行喔',
                     href: '',
                     download: 'file/貓貓拼圖/貓貓拼圖.apk',
@@ -107,7 +117,7 @@ const app = Vue.createApp({
         },
         onSearch(product) {
             if (this.search) {
-                if (product.title.includes(this.search) || product.description.includes(this.search)) {
+                if (product.title.includes(this.search) || product.description.includes(this.search) || product.class.some(x => x.includes(this.search))) {
                     return true
                 } else {
                     return false
